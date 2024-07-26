402195afa3d88fd81c8ca62ed493a078448b3815
Puneet Velidi <psv23@cornell.edu>
2024-07-26 00:11:25 -0400

    working

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -3,11 +3,11 @@ import 'dart:ui';
 import 'package:matrix/matrix.dart';
 
 abstract class AppConfig {
-  static String _applicationName = 'FluffyChat';
+  static String _applicationName = 'THECORTEX';
   static String get applicationName => _applicationName;
   static String? _applicationWelcomeMessage;
   static String? get applicationWelcomeMessage => _applicationWelcomeMessage;
-  static String _defaultHomeserver = 'matrix.org';
+  static String _defaultHomeserver = 'server.cortexflex.org';
   static String get defaultHomeserver => _defaultHomeserver;
   static double fontSizeFactor = 1;
   static const Color chatColor = primaryColor;
